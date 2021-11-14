fabea8d01896ec7ad9e3f989cd1d0df7f6e50901
sbronwyn <sbronwyn@student.21-school.ru>
2021-11-14 18:32:06 +0300

    initial commit

---
+++ philo/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbronwyn <sbronwyn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/14 18:26:47 by sbronwyn          #+#    #+#             */
+/*   Updated: 2021/11/14 18:30:39 by sbronwyn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+int	main(int argc, char **argv)
+{
+	return (0);
+}
